356ca08dc7388bb98a7e9d6c36123025f3454851
achingbrain <alex@achingbrain.net>
2015-10-08 08:00:15 +0100

    use moment

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-var strip = require('rpi-ws281x-native'),
-  Clock = require('./lib/Clock')
+var moment = require('moment')
+var Clock = require('./lib/Clock')
+var wheel = require('./lib/animations/wheel')
 
+var endTime = new Date('2015-10-11T12:00:00');
 var clock = new Clock()
 
 setInterval(function() {
-  var date = new Date()
+  var remaining = moment('2015-10-11T12:00:00').subtract(Date.now()).get("date")
 
-  clock.setTime(date.getHours(), date.getMinutes(), date.getSeconds())
+  clock.setTime(remaining.get('hours'), remaining.get('minutes'), remaining.get('seconds'), wheel)
 }, 500)
