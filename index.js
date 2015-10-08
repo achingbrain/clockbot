459aaa083a80a0e838725283e0ca5dff51149720
achingbrain <alex@achingbrain.net>
2015-10-08 08:24:48 +0100

    vary colour

--- index.js
+++ index.js
@@ -6,7 +6,18 @@ var endTime = new Date('2015-10-11T12:00:00');
 var clock = new Clock()
 
 setInterval(function() {
-  var remaining = moment('2015-10-11T12:00:00').subtract(Date.now()).get("date")
+  var remaining = moment('2015-10-11T12:00:00').subtract(Date.now())
+  var offset = 0
 
-  clock.setTime(remaining.get('hours'), remaining.get('minutes'), remaining.get('seconds'), wheel)
+  var colour = function (pin) {
+    offset++
+
+    if (offset === 255) {
+      offset = 0
+    }
+
+    return wheel(pin + offset)
+  }
+
+  clock.setTime(remaining.get('hours'), remaining.get('minutes'), remaining.get('seconds'), colour)
 }, 500)
